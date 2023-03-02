f53f704b57680c908b22b8e94b8ee7042c445123
Johannahsolem <114469005+Johannahsolem@users.noreply.github.com>
2023-03-02 11:53:46 +0100

    Add files via upload

--- Nippon/Nippon/script.js
+++ Nippon/Nippon/script.js
@@ -1,3 +1,15 @@
 function minFunktion(){
     alert("HUSK at tilmelde dig vores nyhedsbrev for 15% på din ordre!");
 }
+
+minFunktion();
+
+document.addEventListener("click", minFunktion)
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
